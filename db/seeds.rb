0af518f95d50b2bef2bc3afcb80e28448355af65
Hammam Firdaus <hammamxalf@gmail.com>
2024-12-02 10:25:55 +0700

    feat: create seeder user for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+(1..20).each do |i|
+    User.create!(name: Faker::Name.name)
+end
